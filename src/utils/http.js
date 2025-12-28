933da81482eea010523e5b02c0cd7e277a679400
HibaMB <hibabanat.95@gmail.com>
2025-12-28 18:06:05 +0300

    useMutation to handle submitting data, handle success after posting data, invalidate queries to refetch data, adding QueryClient in util file to use in the invalidateQueries

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,3 +1,7 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
+
 export async function fetchEvents({ signal, searchTerm }) {
   let url = "http://localhost:3000/events";
   if (searchTerm) {
@@ -15,4 +19,40 @@ export async function fetchEvents({ signal, searchTerm }) {
   const { events } = await response.json();
 
   return events;
+}
+
+export async function createNewEvent(data) {
+  let url = "http://localhost:3000/events";
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while creating the event");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+  return event;
+}
+
+export async function fetchSelectableImages() {
+  let url = "http://localhost:3000/events/images";
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const error = new Error("An error occured while fetching images");
+    error.status = response.status;
+    error.info = await response.json();
+
+    throw error;
+  }
+
+  const { images } = await response.json();
+  return images;
 }
