6f3548c637cf4de246bf2c453c0a153e95469910
HibaMB <hibabanat.95@gmail.com>
2025-12-28 22:26:19 +0300

    handle updateEvent, use Optimistic update to update record and rollback in case failed on BE, use queryClient to cancel ongoing queries, store previous value, store updated event (optimistically)

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,27 +1,98 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { fetchEvent, queryClient, updateEvent } from "../../utils/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
+  const id = params["id"];
 
-  function handleSubmit(formData) {}
+  const {
+    data: event,
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["events", { id }],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ["events", { id }] }); // to cancel any ongoing queries for this key
+
+      const previousEvent = queryClient.getQueryData(["events", { id }]); // to be used for rollback in case mutation has failed
+
+      queryClient.setQueryData(["events", { id }], newEvent);
+
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", { id }], context.previousEvent);
+    },
+    onSettled: () => {
+      //to make sure all queries in sync with BE, regardless failed or success
+      queryClient.invalidateQueries(["events"]);
+    },
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries({
+    //     queryKey: ["events"],
+    //     refetchType: "none",
+    //   });
+    //   navigate("/events");
+    // },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id, event: formData });
+    navigate("../");
+  }
 
   function handleClose() {
-    navigate('../');
+    navigate("../");
   }
 
   return (
     <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
+      {isPending && (
+        <div className="center">
+          <LoadingIndicator />
+        </div>
+      )}
+      {isError && (
+        <>
+          <ErrorBlock
+            title="An error occured"
+            message={
+              error.info?.message ||
+              "Failed to fetch event details. Please try again later."
+            }
+          />
+          <div className="form-actions">
+            <Link to="../" className="button">
+              Okay
+            </Link>
+          </div>
+        </>
+      )}
+      {event && (
+        <EventForm inputData={event} onSubmit={handleSubmit}>
+          <Link to="../" className="button-text">
+            Cancel
+          </Link>
+          <button type="submit" className="button">
+            Update
+          </button>
+        </EventForm>
+      )}
     </Modal>
   );
 }
